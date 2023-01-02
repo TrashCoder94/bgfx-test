03329b4c75bac22407b575c9e00ddc2d5c01b138
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-02 18:36:26 +0000

    Update Git-Add-Submodule.bat
    Corrected folder structure (for any future references)

--- Scripts/Local/Git-Add-Submodule.bat
+++ Scripts/Local/Git-Add-Submodule.bat
@@ -4,8 +4,8 @@ pushd %~dp0\..\..\
 set /P repo=What git repo do you want to add to BGFXTest/ThirdParty (please reply with .git link not just the normal url)?
 set /P folderName=What do you want to call the parent folder?
 
-echo Adding %repo% to bgfx-test/ThirdParty/%folderName%/
-git submodule add %repo% bgfx-test/ThirdParty/%folderName%/
+echo Adding %repo% to ThirdParty/%folderName%/
+git submodule add %repo% ThirdParty/%folderName%/
 
 popd
 PAUSE
