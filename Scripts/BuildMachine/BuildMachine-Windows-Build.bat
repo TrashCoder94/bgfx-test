2bf9e7b0f3d88633ff0fd3b245c1328fc1aaafb9
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-05 21:40:40 +0000

    Update: Build Machine Scripts
    Changed to use devenv instead of msbuild and correcting directory location for pushd - needed an extra level which is why the build machine couldn't find the premake binary

--- Scripts/BuildMachine/BuildMachine-Windows-Build.bat
+++ Scripts/BuildMachine/BuildMachine-Windows-Build.bat
@@ -1,16 +1,17 @@
 @echo off
-pushd %~dp0\..\
+pushd %~dp0\..\..\
 
 echo "Generating Project with Premake"
 call ThirdParty\premake\Binaries\premake5.exe vs2022
+devenv BGFXTest.sln /rebuild Debug /project Premake
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Building Debug Configuration"
-msbuild BGFXTest.sln /t:Rebuild /p:Configuration=Debug
+devenv BGFXTest.sln /rebuild Debug
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Building Release Configuration"
-msbuild BGFXTest.sln /t:Rebuild /p:Configuration=Release
+devenv BGFXTest.sln /rebuild Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 popd
