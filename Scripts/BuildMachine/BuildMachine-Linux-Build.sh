99d7626e74e9b5b4d0c56204bd63e77faa07f89d
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-11 20:21:38 +0000

    Update BuildMachine-Linux-Build.sh
    AHA!! Looks like premake was appending the platform tag to the configuration (not sure why). Adding the platform tag to the configuration to see if this builds correctly on linux. :eyes:

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -3,6 +3,5 @@
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
-cat Makefile
-make config=Debug
-make config=Release
+make config=debug_x86_64
+make config=release_x86_64
