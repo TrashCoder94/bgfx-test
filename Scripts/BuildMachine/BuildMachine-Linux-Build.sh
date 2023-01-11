73907cc70ed66e42ab7f0c868112c0aff0f1943b
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-11 20:14:54 +0000

    Update BuildMachine-Linux-Build.sh
    Printing out contents of the Makefile - maybe, debugging linux via the buildmachine sucks

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -3,5 +3,6 @@
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
+cat Makefile
 make config=Debug
 make config=Release
