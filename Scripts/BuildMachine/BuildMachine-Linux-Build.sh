cc7f4cb38ca24d3e61505b483f03dcb38ffde68e
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-13 20:07:49 +0000

    Update BuildMachine-Linux-Build.sh
    Install GL for linux

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt install libgl-dev
+
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
