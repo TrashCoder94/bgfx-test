b390ab1cdeeb6ccce53270540648a4fbf31250af
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-13 22:00:28 +0000

    Update BuildMachine-Linux-Build.sh
    x11 stuff hopefully

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 sudo apt-get update
-#sudo apt-get install x11-xserver-utils
+sudo apt-get install x11-xserver-utils
 sudo apt install libgl-dev
-#sudo apt install libxcursor-dev
+sudo apt install libxcursor-dev
 #sudo apt install libx11-dev
 
 echo "Generating Project with Premake!"
