a6f389612ebba328cdefd6764a59d0edac985e03
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-13 20:24:11 +0000

    Update BuildMachine-Linux-Build.sh
    Extending this to the x11 library instead since I need other files too!

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt install libgl-dev
-sudo apt install libxcursor-dev
+sudo apt install libx11-dev
 
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
