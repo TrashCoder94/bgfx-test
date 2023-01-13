bf6d97ac63ef534ee12012ec1c37dfe52fcf31cd
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-13 20:38:40 +0000

    Update BuildMachine-Linux-Build.sh
    Installing XCursor again since it isn't included with the libx11-dev. Trying to setup an early exit if building debug/release config fails.

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
 sudo apt install libgl-dev
+sudo apt install libxcursor-dev
 sudo apt install libx11-dev
 
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
+
 make config=debug_x86_64
+if [ $? -ne 0 ] then
+echo "Building debug configuration failed, exiting out..."
+exit 1
+fi
+
 make config=release_x86_64
+if [ $? -ne 0 ] then
+echo "Building release configuration failed, exiting out..."
+exit 1
+fi
