6115b28e8cb399ddec3a55f1adedc4afe66ca39a
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-14 09:32:03 +0000

    Update BuildMachine-Linux-Build.sh
    Correcting if statement syntax

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -11,13 +11,15 @@ chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
 
 make config=debug_x86_64
-if [ $? -ne 0 ] then
+if [ $? -ne 0 ] 
+then
 echo "Building debug configuration failed, exiting out..."
 exit 1
 fi
 
 make config=release_x86_64
-if [ $? -ne 0 ] then
+if [ $? -ne 0 ] 
+then
 echo "Building release configuration failed, exiting out..."
 exit 1
 fi
