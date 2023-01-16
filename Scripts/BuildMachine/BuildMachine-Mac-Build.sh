9f7146565d992920d6d9f164147e97ec5c166b4c
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-16 20:35:45 +0000

    Update BuildMachine-Mac-Build.sh
    Time to try and diagnose what's going on!

--- Scripts/BuildMachine/BuildMachine-Mac-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build.sh
@@ -4,14 +4,14 @@ echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5_mac
 ThirdParty/premake/Binaries/premake5_mac --file=premake5.lua gmake2
 
-make config=debug_x86_64
+make verbose=yes config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, exiting out..."
 exit 1
 fi
 
-make config=release_x86_64
+make verbose=yes config=release_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building release configuration failed, exiting out..."
