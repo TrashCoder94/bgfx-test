0aee46e779f851fa5c8a1a17a8a0932a784c45da
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-20 19:09:32 +0000

    Update BuildMachine-Mac-Run.sh
    Correcting filepath

--- Scripts/BuildMachine/BuildMachine-Mac-Run.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Running Debug HelloWorld executable!"
-chmod +x Binaries/Debug-mac-x86_64/HelloWorld/HelloWorld
-Binaries/Debug-mac-x86_64/HelloWorld/HelloWorld
+chmod +x Binaries/Debug-macosx-x86_64/HelloWorld/HelloWorld
+Binaries/Debug-macosx-x86_64/HelloWorld/HelloWorld
 if [ $? -ne 0 ] 
 then
 echo "Running debug executable failed, exiting out..."
@@ -10,8 +10,8 @@ exit 1
 fi
 
 echo "Running Release HelloWorld executable!"
-chmod +x Binaries/Release-mac-x86_64/HelloWorld/HelloWorld
-Binaries/Release-mac-x86_64/HelloWorld/HelloWorld
+chmod +x Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
+Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
 if [ $? -ne 0 ] 
 then
 echo "Running release executable failed, exiting out..."
