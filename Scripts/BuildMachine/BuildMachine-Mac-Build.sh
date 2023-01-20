cd12532b5bfe287ac013ca996489d443dce64238
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-20 19:40:10 +0000

    Update: Scripts
    Commented out release and just building/testing debug for faster iteration.
    Online answers seem to suggest installing xquartz to be able to us xvfb-run command.

--- Scripts/BuildMachine/BuildMachine-Mac-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build.sh
@@ -8,15 +8,12 @@ make config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, listing files and then exiting out..."
-#find . -type f
 exit 1
 fi
 
-make config=release_x86_64
-if [ $? -ne 0 ] 
-then
-echo "Building release configuration failed, exiting out..."
-exit 1
-fi
-
-
+#make config=release_x86_64
+#if [ $? -ne 0 ] 
+#then
+#echo "Building release configuration failed, exiting out..."
+#exit 1
+#fi
--- Scripts/BuildMachine/BuildMachine-Mac-Run.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Run.sh
@@ -9,11 +9,11 @@ echo "Running debug executable failed, exiting out..."
 exit 1
 fi
 
-echo "Running Release HelloWorld executable!"
-chmod +x Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
-Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
-if [ $? -ne 0 ] 
-then
-echo "Running release executable failed, exiting out..."
-exit 1
-fi
+#echo "Running Release HelloWorld executable!"
+#chmod +x Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
+#Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
+#if [ $? -ne 0 ] 
+#then
+#echo "Running release executable failed, exiting out..."
+#exit 1
+#fi
