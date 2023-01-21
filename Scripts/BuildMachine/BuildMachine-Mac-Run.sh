c3df5bff8f1891a01b253367b984696165a113f4
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-21 19:49:44 +0000

    Update BuildMachine-Mac-Run.sh
    Removing exit on error checks to see if running the exe goes any further

--- Scripts/BuildMachine/BuildMachine-Mac-Run.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Run.sh
@@ -3,11 +3,11 @@
 echo "Running Debug HelloWorld executable!"
 chmod +x Binaries/Debug-macosx-x86_64/HelloWorld/HelloWorld
 Binaries/Debug-macosx-x86_64/HelloWorld/HelloWorld
-if [ $? -ne 0 ] 
-then
-echo "Running debug executable failed, exiting out..."
-exit 1
-fi
+#if [ $? -ne 0 ] 
+#then
+#echo "Running debug executable failed, exiting out..."
+#exit 1
+#fi
 
 #echo "Running Release HelloWorld executable!"
 #chmod +x Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
